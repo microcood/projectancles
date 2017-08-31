924b498b5a3d1d655e8490139c1d626f8ca4ef7d
Micwaits Microcood <microcood@gmail.com>
2017-08-31 14:32:06 +0300

    Dropping tables instead of deleting db

--- api/tests.py
+++ api/tests.py
@@ -15,29 +15,17 @@ from utils import get_component
 from projects.tests import *
 
 
-def create_test_db(engine):
-    conn = engine.connect()
-    conn.execute("COMMIT")
-    conn.execute("CREATE DATABASE test_database")
-    conn.close()
-
-
-def remove_test_db(engine):
-    conn = engine.connect()
-    conn.execute("COMMIT")
-    conn.execute("DROP DATABASE test_database")
-    conn.close()
-
-
 @pytest.fixture(scope='session')
 def session(request):
     backend = get_component(SQLAlchemyBackend)
-    engine = backend.engine
+
     try:
-        create_test_db(engine)
+        conn = backend.engine.connect()
+        conn.execute("COMMIT")
+        conn.execute("CREATE DATABASE test_database")
+        conn.close()
     except Exception as e:
-        remove_test_db(engine)
-        create_test_db(engine)
+        pass
 
     engine_new = create_engine(
         'postgresql://{e[DB_USER]}:{e[DB_PASSWORD]}@{e[DB_HOST]}/test_database'  # nopep8
@@ -48,16 +36,14 @@ def session(request):
     backend.engine = engine_new
     backend.Session = sessionmaker(bind=engine_new)
     Base.metadata.create_all(engine_new)
-    # session = backend.Session()
+    session = backend.Session()
 
     def teardown():
-        try:
-            remove_test_db(engine)
-        except Exception as e:
-            pass
+        session.close_all()
+        Base.metadata.drop_all(engine_new)
 
     request.addfinalizer(teardown)
-    return backend.Session()
+    return session
 
 
 @pytest.fixture(scope='session')
