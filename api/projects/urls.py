5e28e529c992b6e8b76659b348ffd8537e1c4171
Micwaits Microcood <microcood@gmail.com>
2017-08-31 19:24:20 +0300

    Basic CRUD functionality

--- api/projects/urls.py
+++ api/projects/urls.py
@@ -5,4 +5,6 @@ projects_urls = [
     Route('/', 'GET', views.get_projects_list),
     Route('/', 'POST', views.create_project),
     Route('/{project_id}', 'GET', views.get_project),
+    Route('/{project_id}', 'PATCH', views.update_project),
+    Route('/{project_id}', 'DELETE', views.delete_project),
 ]
--- api/projects/views.py
+++ api/projects/views.py
@@ -1,17 +1,18 @@
+from apistar import http
 from apistar.backends.sqlalchemy_backend import Session
 from .models import Project
 from utils import get_component
 
 
-def create_project(name: str):
+def create_project(data: http.RequestData):
     session = get_component(Session)
-    project = Project(name=name)
+    project = Project(name=data['name'])
     session.add(project)
     session.commit()
-    return {
+    return http.Response({
         'id': project.id,
         'name': project.name
-    }
+    }, status=201)
 
 
 def get_projects_list():
@@ -32,3 +33,26 @@ def get_project(project_id: int):
         'id': project.id,
         'name': project.name
     }
+
+
+def update_project(project_id: int, data: http.RequestData):
+    session = get_component(Session)
+    p = session.query(Project).filter(
+        Project.id == project_id
+    ).first()
+
+    p.name = data['name']
+    session.commit()
+    return {
+        'id': p.id,
+        'name': p.name
+    }
+
+
+def delete_project(project_id: int):
+    session = get_component(Session)
+    session.query(Project).filter(
+        Project.id == project_id
+    ).delete()
+    session.commit()
+    return http.Response(status=204)
