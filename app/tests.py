999175e5d530214940229d8009ec3323fddcf49d
Micwaits Microcood <microcood@gmail.com>
2017-09-30 04:44:56 +0300

    Prettified tests

--- app/tests.py
+++ app/tests.py
@@ -11,18 +11,23 @@ from sqlalchemy.pool import NullPool
 
 from apistar import Settings
 from apistar.backends.sqlalchemy_backend import SQLAlchemyBackend
+from apistar.test import TestClient
+from apistar.backends.sqlalchemy_backend import Session
+from werkzeug.security import check_password_hash
 
 from app import app
 from db_base import Base
 from utils import get_component
-
 from users.models import User
 from projects.models import Project
-from apistar.test import TestClient
-from apistar.backends.sqlalchemy_backend import Session
 
 
 fake = Faker()
+
+
+@pytest.fixture(scope='session')
+def settings():
+    return get_component(Settings)
 
 
 @pytest.fixture(scope='session')
@@ -56,41 +61,47 @@ def session(request):
     return session
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 def anon_client():
     return TestClient(app)
 
 
 @pytest.fixture(scope='session')
-def client():
+def client(settings):
     clnt = TestClient(app)
-    settings = get_component(Settings)
     token = jwt.encode(
         {'user_id': 1234}, settings['JWT_SECRET'], algorithm='HS256')
     clnt.headers.update(
         {'Authorization': 'Bearer {}'.format(token.decode('utf-8'))})
-    print(clnt.headers)
     return clnt
 
 
 class BaseTestViewSet(object):
-    def mock_obj(self):
+    def _mock_obj(self):
         return {}
 
-    def create_obj(self, session):
-        obj = self.model(**self.mock_obj())
+    def _create_obj(self, session):
+        obj = self.model(**self._mock_obj())
         session.add(obj)
         session.commit()
         return obj
 
-    def test_list_empty(self, session: Session, client: TestClient):
-        response = client.get('/{}/'.format(self.url))
-        assert response.status_code == 200
-        assert response.json() == []
+    @pytest.fixture(scope='function')
+    def new_obj(self, session: Session):
+        return self._create_obj(session)
+
+    @pytest.fixture(scope='function')
+    def mock(self):
+        return self._mock_obj()
+
+    @pytest.fixture(scope='function')
+    def clean_db(self, session):
+        session.query(self.model).delete()
+        session.commit()
 
     def test_list(self, session: Session, client: TestClient):
-        obj = self.create_obj(session)
-        obj2 = self.create_obj(session)
+        obj = self._create_obj(session)
+        obj2 = self._create_obj(session)
 
         response = client.get('/{}/'.format(self.url))
 
@@ -100,77 +111,85 @@ class BaseTestViewSet(object):
             obj2.render()
         ]
 
-    def test_create(self, session: Session, client: TestClient):
-        fake_obj = self.mock_obj()
+    def test_list_empty(self, clean_db, client: TestClient):
+        response = client.get('/{}/'.format(self.url))
+
+        assert response.status_code == 200
+        assert response.json() == []
+
+    def test_create(self, mock: dict, session: Session, client: TestClient):
         response = client.post(
             '/{}/'.format(self.url),
-            data=fake_obj
+            data=mock
         )
-
         resp_obj = response.json()
-
-        obj = session.query(self.model).filter(
-            self.model.id == resp_obj['id']
-        ).first()
+        obj = session.query(self.model).filter_by(id=resp_obj['id']).first()
 
         assert response.status_code == 201
         assert response.json() == obj.render()
 
-    def test_view_one(self, session: Session, client: TestClient):
-        obj = self.create_obj(session)
+    def test_view_one(self, new_obj: Base, client: TestClient):
+        response = client.get('/{}/{}'.format(self.url, new_obj.id))
 
-        response = client.get('/{}/{}'.format(self.url, obj.id))
         assert response.status_code == 200
-        assert response.json() == obj.render()
+        assert response.json() == new_obj.render()
 
-    def test_delete(self, session: Session, client: TestClient):
-        obj = self.create_obj(session)
-
-        query = session.query(self.model).filter(
-            self.model.id == obj.id
-        )
-
-        response = client.delete('/{}/{}'.format(self.url, obj.id))
+    def test_delete(self, new_obj: Base, session: Session, client: TestClient):
+        query = session.query(self.model).filter_by(id=new_obj.id)
+        response = client.delete('/{}/{}'.format(self.url, new_obj.id))
 
         assert response.status_code == 204
         assert response.text == ""
         assert query.count() == 0
 
-    def test_update(self, session: Session, client: TestClient):
-        obj = self.create_obj(session)
-
-        new_mock = self.mock_obj()
+    def test_update(
+        self,
+        new_obj: Base,
+        mock: dict,
+        session: Session,
+        client: TestClient
+    ):
         response = client.patch(
-            '/{}/{}'.format(self.url, obj.id),
-            data=new_mock
+            '/{}/{}'.format(self.url, new_obj.id),
+            data=mock
         )
 
-        new_obj = dict(obj)
-        for k in new_obj.keys():
-            if new_mock.get(k):
-                new_obj[k] = new_mock.get(k)
+        mock['id'] = new_obj.id
+        session.refresh(new_obj)
 
-        session.refresh(obj)
-        assert dict(obj) == new_obj
         assert response.status_code == 200
-        assert response.json() == obj.render()
+        assert response.json() == self.model(**mock).render()
+        assert response.json() == new_obj.render()
 
-    def test_nonexistance(self, client: TestClient):
-        res = client.get('/{}/{}'.format(self.url, 12344123))
-        res1 = client.patch('/{}/{}'.format(self.url, 12344123))
-        res2 = client.delete('/{}/{}'.format(self.url, 12344123))
+    @pytest.mark.parametrize("method, id", [
+        ("GET", 3),
+        ("PATCH", 3),
+        ("DELETE", 3),
+    ])
+    def test_not_found(self, clean_db, client: TestClient, method, id):
+        res = client.request(method, '/{}/{}'.format(self.url, id))
 
-        assert res.status_code == res1.status_code == res2.status_code == 404
-        assert res.json() == res1.json() == res2.json() == {
-            "message": "Not found"
-        }
+        assert res.status_code == 404
+        assert res.json() == {"message": "Not found"}
+
+    @pytest.mark.parametrize("method, url", [
+        ("GET", '/{}/3'),
+        ("POST", '/{}/'),
+        ("PATCH", '/{}/3'),
+        ("DELETE", '/{}/3'),
+    ])
+    def test_not_authenticated(self, anon_client: TestClient, method, url):
+        res = anon_client.request(method, url.format(self.url))
+
+        assert res.status_code == 401
+        assert res.json() == {"message": "Not authenticated"}
 
 
 class TestUserViewSet(BaseTestViewSet):
     url = 'users'
     model = User
 
-    def mock_obj(self):
+    def _mock_obj(self):
         return {
             "first_name": fake.first_name(),
             "last_name": fake.last_name(),
@@ -178,77 +197,74 @@ class TestUserViewSet(BaseTestViewSet):
             "email": fake.email()
         }
 
-    def test_password(self, session: Session, client: TestClient):
-        from werkzeug.security import check_password_hash
-
-        fake_obj = self.mock_obj()
+    def test_password(self, mock, session: Session, client: TestClient):
         response = client.post(
             '/{}/'.format(self.url),
-            data=fake_obj
+            data=mock
         )
         user_obj = response.json()
-        user = session.query(User).filter(User.id == user_obj['id']).first()
-        assert check_password_hash(str(user.password), fake_obj['password'])
+        user = session.query(User).filter_by(id=user_obj['id']).first()
 
-    def test_password_not_exposed(self, session: Session, client: TestClient):
-        obj = self.create_obj(session)
-        response = client.get('/{}/{}'.format(self.url, obj.id))
-        resp_obj = response.json()
-        assert not resp_obj.get('password')
+        assert check_password_hash(str(user.password), mock['password'])
 
-    # def test_django_password_validation(self)
+    def test_password_not_exposed(self, new_obj, client: TestClient):
+        response = client.get('/{}/{}'.format(self.url, new_obj.id))
+
+        assert not response.json().get('password')
+
+    def test_user_bound_token(
+        self,
+        settings: Settings,
+        new_obj: Base,
+        client: TestClient
+    ):
+        response = client.post('/tokens/', data={
+            "grant_type": "password",
+            "username": new_obj.email,
+            "password": new_obj.password,
+        })
+        response_dict = response.json()
+        jwt_payload = jwt.decode(
+            response_dict['access_token'],
+            settings['JWT_SECRET'],
+            algorithms=['HS256']
+        )
+
+        assert response.status_code == 200
+        assert jwt_payload == {
+            "user_id": new_obj.id,
+            "exp": response_dict['expires_in']
+        }
+        assert response_dict['expires_in'] > int(datetime.now().timestamp())
+
+    def test_authorization_flow(
+        self,
+        new_obj: Base,
+        session: Session,
+        anon_client: TestClient
+    ):
+        response1 = anon_client.get('/projects/')
+        response2 = anon_client.post('/tokens/', data={
+            "grant_type": "password",
+            "username": new_obj.email,
+            "password": new_obj.password,
+        })
+        token = response2.json().get('access_token')
+        anon_client.headers.update({
+            'Authorization': 'Bearer {}'.format(token)
+        })
+        response3 = anon_client.get('/projects/')
+
+        assert response1.status_code == 401
+        assert response2.status_code == 200
+        assert response3.status_code == 200
 
 
 class TestProjectsViewSet(BaseTestViewSet):
     url = 'projects'
     model = Project
 
-    def mock_obj(self):
+    def _mock_obj(self):
         return {
             "name": fake.word(),
         }
-
-
-def test_token(session: Session, client: TestClient):
-    settings = get_component(Settings)
-    JWT_SECRET = settings['JWT_SECRET']
-
-    user = TestUserViewSet().create_obj(session)
-
-    response = client.post('/tokens/', data={
-        "grant_type": "password",
-        "username": user.email,
-        "password": user.password,
-    })
-    response_dict = response.json()
-    jwt_payload = jwt.decode(
-        response_dict['access_token'], JWT_SECRET, algorithms=['HS256'])
-
-    assert response.status_code == 200
-    assert jwt_payload == {
-        "user_id": user.id,
-        "exp": response_dict['expires_in']
-    }
-    assert response_dict['expires_in'] > int(datetime.now().timestamp())
-
-
-def test_authorization_flow(session: Session, anon_client: TestClient):
-    user = TestUserViewSet().create_obj(session)
-    response = anon_client.get('/projects/')
-
-    assert response.status_code == 401
-    assert response.json() == {"message": "Not authenticated"}
-
-    response = anon_client.post('/tokens/', data={
-        "grant_type": "password",
-        "username": user.email,
-        "password": user.password,
-    })
-
-    token = response.json().get('access_token')
-    anon_client.headers.update(
-        {'Authorization': 'Bearer {}'.format(token)})
-
-    response = anon_client.get('/projects/')
-    assert response.status_code == 200
-    assert type(response.json()) == list
